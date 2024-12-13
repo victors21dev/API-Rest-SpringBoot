c0eecb272a768d3dd9893ac40fb8b0083e297ecf
Victor Santos <santos.victors2000@gmail.com>
2024-12-13 20:14:26 -0300

    Commit initial

---
+++ src/test/java/victor_API/API_Rest/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package victor_API.API_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
